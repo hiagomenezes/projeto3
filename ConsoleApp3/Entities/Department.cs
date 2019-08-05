295c836a58553e582b23dc4c57661c9aaf7f0462
hiagomenezes <hiago@itecnologia.com.br>
2019-08-05 16:39:14 -0300

    fim do projeto

---
+++ ConsoleApp3/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3.Entities
+{
+    class Department
+    {
+        public String Name { get; set; }
+
+        public Department()
+        {
+        }
+            
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
